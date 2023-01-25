03b482ab6ce49500ab0160e629a50d1d773fafd4
Jinay Gada <jinaygada18@gmail.com>
2023-01-25 11:32:57 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+mongoose.set('strictQuery', false);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password']});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get('/', (req,res)=>{
+    res.render("home");
+});
+
+app.get("/login", (req,res)=>{
+    res.render("login");
+});
+
+app.get("/register", (req,res)=>{
+    res.render("register");
+});
+
+app.post("/register", (req,res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    User.findOne({email: req.body.username}, (err,foundUser)=>{
+        if(foundUser){
+            console.log("user already exists use different email address.");
+            res.redirect("/register");
+        }else{
+            newUser.save((err)=>{
+                if(!err){
+                    res.render("secrets");      
+                }else{
+                    res.render(err);
+                }
+            });
+        }
+        if(err){
+            console.log(err);
+        }
+    });
+})
+
+
+app.post("/login", (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    User.findOne({email: username }, (err, foundUser)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }else{
+                    res.redirect("/login");
+                }
+            }
+        }
+    });
+});
+
+
+app.listen(3000, ()=>{
+    console.log("Server started on port 3000")
+});
